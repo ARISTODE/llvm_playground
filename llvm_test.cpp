548fa3dea65c506a06861a0204c9ff274736332e
Yongzhe Huang <jk.aristode@gmail.com>
2021-03-02 10:49:39 -0500

    add demo code for use pdg as a lib

--- llvm_test.cpp
+++ llvm_test.cpp
@@ -11,12 +11,23 @@
 #include "llvm/IR/DebugInfo.h"
 #include "llvm/IR/GlobalVariable.h"
 #include "llvm/IR/CFG.h"
+#include "llvm/InitializePasses.h"
 
 #include "llvm/Support/raw_ostream.h"
 #include "llvm/IR/Function.h"
 #include "llvm/IR/Instructions.h" 
 #include "llvm/IR/InstIterator.h"
 #include "llvm/IR/InstrTypes.h"
+#include "ProgramDependencyGraph.hh"
+// svf
+/* #include "SVF-FE/PAGBuilder.h" */
+/* #include "WPA/Andersen.h" */
+/* #include "SVF-FE/LLVMUtil.h" */
+// sea_dsa
+// #include "seadsa/CompleteCallGraph.hh"
+// #include "seadsa/CallGraphUtils.hh"
+// #include "seadsa/DsaAnalysis.hh"
+// #include "seadsa/Global.hh"
 
 #include <vector>
 #include <set>
@@ -27,28 +38,60 @@
 
 namespace {
     using namespace llvm;
+    /* using namespace SVF; */
 
     class llvmTest : public ModulePass {
-    public:
-        static char ID;
+        public:
+            static char ID;
 
-        llvmTest() : ModulePass(ID) {
-        }
+            llvmTest() : ModulePass(ID) {
+            }
 
-        bool runOnModule(Module &M)
-        {
-            // do something with the module
-            return false;
-        }
+            bool runOnModule(Module &M) override
+            {
+                auto PDG = getAnalysis<pdg::ProgramDependencyGraph>().getPDG();
+                for (auto &F : M) 
+                {
+                    if (F.isDeclaration())
+                        continue;
+                    for (auto instI = inst_begin(F); instI != inst_end(F); ++instI) {
+                        pdg::Node* n = PDG->getNode(*instI);
+                        if (n == nullptr)
+                            continue;
+                        // print neighbors
+                        auto inEdges = n->getInEdgeSet();
+                        auto outEdges = n->getOutEdgeSet();
+                        for (auto inEdge : inEdges) {
+                            if (inEdge->getEdgeType() == pdg::EdgeType::DATA_DEF_USE)
+                                errs() << "find def use in neighbor: " << *inEdge->getSrcNode()->getValue() << "\n";
+                        }
+                        errs() << " =================================================================  \n";
+                        for (auto outEdge : outEdges) {
+                            if (outEdge->getEdgeType() == pdg::EdgeType::DATA_DEF_USE)
+                                errs() << "find def use out neighbor: " << *outEdge->getDstNode()->getValue() << "\n";
+                        }
 
+                    }
+                }
 
-        // Analysis Usage, specify PDG at this time
-        void getAnalysisUsage(AnalysisUsage &AU) const {
-            AU.setPreservesAll();
-        }
+                return false;
+            }
+            llvm::StringRef getPassName() const override { return "LLVM Test"; }
+            // Analysis Usage, specify PDG at this time
+            void getAnalysisUsage(AnalysisUsage &AU) const override
+            {
+                AU.addRequired<pdg::ProgramDependencyGraph>();
+                AU.setPreservesAll();
+            }
     };
 
     char llvmTest::ID = 0;
-    static RegisterPass<llvmTest> llvmTest("llvm-test", "LLVM TEST", false, true);
+    static RegisterPass<llvmTest> X("llvm-test", "LLVM TEST", false, true);
+    // static void registerTestPassPass(const PassManagerBuilder &, llvm::legacy::PassManagerBase &PM)
+    // {
+    //   PM.add(new seadsa::DsaAnalysis());
+    //   PM.add(new llvmTest());
+    // }
+    // static RegisterStandardPasses RegisterTestPassPass(PassManagerBuilder::EP_EarlyAsPossible, registerTestPassPass);
 } // namespace
 #endif
--- test/build.sh
+++
@@ -1,8 +0,0 @@
-clang -emit-llvm -S -g *.c
-#clang --target=x86_64-unknown-linux-gnu -emit-llvm -S *.c
-for f in *.ll; do
-    llvm-as $f
-done
-
-mv *.ll ./llvm_ll
-mv *.bc ./bitcode
